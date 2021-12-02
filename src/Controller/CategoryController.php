d0d8f103055216c77bf49c5d03a57d38cbd9b16b
Bruno Fernandes <bruno.fernandes3789@gmail.com>
2021-12-02 15:55:22 +0100

    c'est trop bien les crud

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -1,6 +1,5 @@
 <?php
 
-// src/Controller/CategoryController.php
 namespace App\Controller;
 
 use App\Entity\Category;
@@ -8,26 +7,58 @@ use App\Entity\Program;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Form\CategoryType;
+use Symfony\Component\HttpFoundation\Request;
 
 /**
-* @Route("/category", name="category_")
-*/
+ * @Route("/category", name="category_")
+ */
 class CategoryController extends AbstractController
 {
     /**
-     * @Route("/", "name=index")
-     */
+     * @Route("/", name="index")
+     */    
     public function index(): Response
     {
         $categories = $this->getDoctrine()
-             ->getRepository(Category::class)
-             ->findAll();
+            ->getRepository(Category::class)
+            ->findAll();
 
-        return $this->render('category/index.html.twig', [
-        'categories' => $categories,
+        return $this->render(
+            'category/index.html.twig', [
+                'categories' => $categories
         ]);
     }
-
+    /**
+     * The controller for the category add form
+     *
+     * @Route("/new", name="new")
+     */
+    public function new(Request $request) : Response
+    {
+        // Create a new Category Object
+        $category = new Category();
+        // Create the associated Form
+        $form = $this->createForm(CategoryType::class, $category);
+        // Get data from HTTP request
+        $form->handleRequest($request);
+        // Was the form submitted ?
+        if ($form->isSubmitted()) {
+            // Deal with the submitted data
+            // Get the Entity Manager
+            $entityManager = $this->getDoctrine()->getManager();
+            // Persist Category Object
+            $entityManager->persist($category);
+            // Flush the persisted object
+            $entityManager->flush();
+            // Finally redirect to categories list
+            return $this->redirectToRoute('category_index');
+        }
+        // Render the form
+        return $this->render('category/new.html.twig', [
+            "form" => $form->createView()]);
+    }
+    
     /**
      *
      * @Route("/{categoryName}",methods={"GET"}, name="show")
@@ -59,4 +90,5 @@ class CategoryController extends AbstractController
             'programs' => $programs,
         ]);
     }
+
 }
